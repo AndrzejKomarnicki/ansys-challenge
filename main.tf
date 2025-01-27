8fb668a50f1b3f196957e39de1117564a51f3eb8
Andrzej Komarnicki <andrzej_komarnicki@hotmail.com>
2025-01-27 10:23:41 -0500

    create vpc with the specified cidr block

---
+++ main.tf
@@ -0,0 +1,23 @@
+provider "aws" {
+
+  region = var.aws_region
+
+}
+
+
+resource "aws_vpc" "main" {
+
+  cidr_block = var.vpc_cidr
+
+  enable_dns_hostnames = true
+
+  enable_dns_support = true
+
+
+  tags = {
+
+    Name = "${var.project_name}-vpc"
+
+  }
+
+}
