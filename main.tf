0c64d481a486e4a19bea831e14266297ed4672aa
Andrzej Komarnicki <andrzej_komarnicki@hotmail.com>
2025-01-27 11:23:25 -0500

    launch an ec2 instance in a public subnet with http access

--- main.tf
+++ main.tf
@@ -152,4 +152,23 @@ resource "aws_security_group" "web" {
 
 }
 
+resource "aws_instance" "web" {
+
+  ami           = var.instance_ami
+
+  instance_type = var.instance_type
+
+  subnet_id     = aws_subnet.public[0].id
+
+
+  vpc_security_group_ids = [aws_security_group.web.id]
+
+
+  tags = {
+
+    Name = "${var.project_name}-web-server"
+
+  }
+
+}
 
