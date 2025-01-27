60202868d432cdca657af5cf90acee5ea8e5ca5e
Andrzej Komarnicki <andrzej_komarnicki@hotmail.com>
2025-01-27 12:22:19 -0500

    use variables for flexibility and rreusability

---
+++ variables.tf
@@ -0,0 +1,70 @@
+variable "aws_region" {
+
+  type = string
+
+  default = "us-west-2"
+
+}
+
+
+variable "project_name" {
+
+  type = string
+
+  default = "demo"
+
+}
+
+
+variable "vpc_cidr" {
+
+  type = string
+
+  default = "10.0.0.0/16"
+
+}
+
+
+variable "public_subnet_cidrs" {
+
+  type = list(string)
+
+  default = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+
+}
+
+
+variable "private_subnet_cidrs" {
+
+  type = list(string)
+
+  default = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+
+}
+
+
+variable "availability_zones" {
+
+  type = list(string)
+
+  default = ["us-west-2a", "us-west-2b", "us-west-2c"]
+
+}
+
+
+variable "instance_ami" {
+
+  type = string
+
+  default = "ami-0ac4dfaf1c5c0cce9"
+
+}
+
+
+variable "instance_type" {
+
+  type = string
+
+  default = "t3.micro"
+
+}
